3d238efe22edf750401548078752753f62482d37
Muhammad Qasim <muhammad.qasim2378@gmail.com>
2025-08-20 18:58:37 +0500

    set up related

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,9 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "flowbite", to: "https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.turbo.min.js"
+pin "@fortawesome/fontawesome-free", to: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/js/all.min.js" # @7.0.0
+pin "list.js" # @2.3.1
+pin "string-natural-compare" # @3.0.1
+# pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+# pin "flowbite-datepicker" # @1.3.2
