d7c150848b76bd47fe6efe0944eb943d1483d130
Muhammad Qasim <muhammad.qasim2378@gmail.com>
2025-08-21 16:11:15 +0500

    rm string

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,6 +9,5 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "flowbite", to: "https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.turbo.min.js"
 # config/importmap.rb
 pin "list.js", to: "https://esm.sh/list.js@2.3.1"
-pin "string-natural-compare", to: "https://cdn.jsdelivr.net/npm/string-natural-compare@3.0.1/index.js"
 # Optional: FontAwesome (JS version, if needed)
 pin "@fortawesome/fontawesome-free", to: "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.5.2/js/all.min.js"
