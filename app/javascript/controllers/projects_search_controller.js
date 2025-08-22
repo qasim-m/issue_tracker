208ef827c2641a88c7dd6982312f2505a7cbb6bf
Muhammad Qasim <muhammad.qasim2378@gmail.com>
2025-08-22 09:21:10 +0500

    view improvements

--- app/javascript/controllers/projects_search_controller.js
+++ app/javascript/controllers/projects_search_controller.js
@@ -19,7 +19,7 @@ export default class extends Controller {
   initializeListJs() {
     if (this.hasListTarget) {
       this.listJs = new List(this.element, {
-        valueNames: ["title"],
+        valueNames: ["title", "num"],
         listClass: "list",
         page: 10,
         pagination: true,
