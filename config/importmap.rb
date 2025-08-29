d8c7a751ec646927ddc4af7290826f43130bd351
Muhammad Qasim <muhammad.qasim2378@gmail.com>
2025-08-29 21:22:13 +0500

    tailwind css

--- config/importmap.rb
+++ config/importmap.rb
@@ -11,3 +11,5 @@ pin "flowbite", to: "https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.t
 pin "list.js", to: "https://esm.sh/list.js@2.3.1"
 # Optional: FontAwesome (JS version, if needed)
 pin "@fortawesome/fontawesome-free", to: "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.5.2/js/all.min.js"
+pin "@tailwindcss/forms", to: "@tailwindcss--forms.js" # @0.5.10
+pin "tailwindcss/plugin", to: "tailwindcss--plugin.js" # @4.1.12
